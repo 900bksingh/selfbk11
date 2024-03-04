e3276724bff932da3e327eec2231c873c76fb31c
900bksingh <900bksingh@gmail.com>
2024-03-04 12:08:19 +0530

    127:Create save post feature

---
+++ src/main/java/com/selfbk/selfbk11/Selfbk11Application.java
@@ -0,0 +1,13 @@
+package com.selfbk.selfbk11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Selfbk11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Selfbk11Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/selfbk/selfbk11/Selfbk11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selfbk.selfbk11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Selfbk11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
